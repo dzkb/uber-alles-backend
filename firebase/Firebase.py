5957d0d7e5525740d51e37e003def28ccc3f4003
Dzakub <dzakub@gmail.com>
2017-04-08 15:22:56 +0200

    Initial endpoint definition

---
+++ firebase/Firebase.py
@@ -0,0 +1,6 @@
+import requests
+
+class Firebase:
+
+    def __init__(self, server_key):
+        self.server_key = server_key
--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from flask import Flask
+from flask import request
+import requests
+from config.config import Firebase as Firebase_config
+from firebase import Firebase
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello, world!'
+    return Firebase_config.SERVER_KEY
+
+@app.route('/fares/<fare_id>', methods=['DELETE'])
+def handle_fares_id(fare_id):
+    return "to_delete: " + fare_id
+
+@app.route('/fares', methods=['GET', 'POST'])
+def handle_fares():
+    return "fares"
+
+@app.route('/acceptedFares/<fare_id>', methods=['POST', 'DELETE'])
+def handle_accepted_fares_id(fare_id):
+    return "accepted_fares: " + fare_id
+
+@app.route('/acceptedFares', methods=['GET'])
+def handle_accepted_fares():
+    return "accepted_fares"
